9001dae0a934e5941231246280274b55d4170c44
Michael Heydt <mike@heydt.org>
2019-10-09 15:22:43 -0600

    Added text moderation project

---
+++ LinuxAcademy.Azure.AI.ModerateText/LinuxAcademy.Azure.AI.ModerateText/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.CognitiveServices.ContentModerator;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinuxAcademy.Azure.AI.ModerateText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string endpoint = ; // insert your endpoint
+            string key = ; // insert your key
+
+            var client = new ContentModeratorClient(
+                new ApiKeyServiceClientCredentials(key))
+            {
+                Endpoint = endpoint
+            };
+
+            using (var stream = File.OpenRead("TextFile1.txt"))
+            {
+                var screenResult = client.TextModeration.ScreenText(
+                    "text/plain",
+                    stream,
+                    "eng",
+                    autocorrect: false,
+                    pII: false,
+                    listId: null,
+                    classify: false);
+
+                Console.WriteLine(JsonConvert.SerializeObject(screenResult, Formatting.Indented));
+            }
+        }
+    }
+}
